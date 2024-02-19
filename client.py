640369e94a7131607ed18dc88753edb9400d9ecf
Mitchell Hashimoto <andere@uchicago.edu>
2024-02-19 10:28:12 +0100

    pdf working

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, send_file
+from weasyprint import HTML
 from functools import wraps
 import re
 from helpers import download_youtube_transcript, get_youtube_title
@@ -7,6 +8,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 from urllib.parse import urlparse, parse_qs
 from concurrent.futures import ThreadPoolExecutor
+import markdown2
 
 load_dotenv()
 client = OpenAI()
@@ -72,16 +74,16 @@ def get_transcription(video_id):
     formatted_transcript = llm_request(format_prompt(transcript_raw))
     return jsonify({'transcription': formatted_transcript})
 
-@app.route('/markdown_download', methods=['GET'])
+@app.route('/download', methods=['GET'])
 @validate_video
-def markdown_download(video_id):
+def download_content(video_id):
+    format_type = request.args.get('format', 'markdown').lower()
     video_title = get_youtube_title(video_id)
     transcript_raw = download_youtube_transcript(video_id)
 
     def fetch_data(prompt):
         return llm_request(prompt(transcript_raw))
     
-    # concurrent requests
     with ThreadPoolExecutor(max_workers=3) as executor:
         future_summary = executor.submit(fetch_data, summary_prompt)
         future_bullet_points = executor.submit(fetch_data, bps_prompt)
@@ -92,11 +94,20 @@ def markdown_download(video_id):
         formatted_transcript = future_formatted_transcript.result()
     
     markdown_content = f"""# {video_title} -- Transcript\n\n## Summary\n{summary}\n\n## Bullet Points\n{bullet_points}\n\n## Formatted Transcript\n{formatted_transcript}\n"""
-    file_path = f"outputs/{video_id}.md"
-    with open(file_path, 'w') as markdown_file:
-        markdown_file.write(markdown_content)
-    return send_file(file_path, as_attachment=True, download_name=f"{video_id}_summary.md")
+    
+    if format_type == 'pdf':
+        html_content = markdown2.markdown(markdown_content)
+        file_path = f"outputs/{video_id}.pdf"
+        HTML(string=html_content).write_pdf(file_path)
+    else:  # Markdown format
+        file_path = f"outputs/{video_id}.md"
+        with open(file_path, 'w') as file:
+            file.write(markdown_content)
+    
+    return send_file(file_path, as_attachment=True, download_name=f"{video_id}_summary.{format_type}")
 
+
+# 404
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({'error': 'This endpoint does not exist. Please check the URL and try again.'}), 404
@@ -105,6 +116,8 @@ def page_not_found(e):
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({'message': 'YB <> LLM => Server running!'})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
